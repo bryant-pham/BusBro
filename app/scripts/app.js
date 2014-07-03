b3dc7b0be4ea807fedb2b0566adbf0db8b03d34d
unknown <bryant.pham@BPHAM81HOU.pariveda.local>
2014-07-03 09:45:54 -0500

    Improvements from working session with Ryan

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,21 +1,11 @@
 var busBroApp = angular.module('busBroApp', []);
 busBroApp.controller('purpleCtrl', function($scope, $http) {
-	$scope.route = null;
-	$scope.keys = [];
-	$scope.routePos = 0;
-	$http.get('scripts/schedule.json').success(function(data) {
-		$scope.purpleMcgovern = data.purple.mcgovern;
-		$scope.purpleBcmMain = data.purple.bcmMain;
-		$scope.purpleJamail = data.purple.jamail;
-		$scope.purpleBcmMedCenter = data.purple.bcmMedCenter;
-
-		angular.forEach(data, function(routes, color){
-			angular.forEach(routes, function(value, routeKey) {
-				$scope.keys.push(routeKey);
-			});
+	$scope.init = function() {
+		$http.get('scripts/schedule.json').success(function(data) {
+			$scope.data = data;
 		});
-		$scope.route = $scope.keys[$scope.routePos];
-	});
+	}
+	
 
 	$scope.nextRoute = function() {
 		if($scope.routePos == ($scope.keys.length-1))
@@ -32,6 +22,6 @@ busBroApp.controller('purpleCtrl', function($scope, $http) {
 			$scope.routePos--;
 		$scope.route = $scope.keys[$scope.routePos];
 	}
-
+	$scope.init();
 });
 
