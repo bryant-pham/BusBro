556ad0d535b23b6bbac871472b20dcd04f8e9bac
Bryant Pham <bryantphamm@gmail.com>
2014-07-22 07:36:43 -0500

    Fixed $$hashkey side effect

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,29 +3,29 @@
 var busBroApp = angular.module('busBroApp', []);
 busBroApp.controller('busBroCtrl', function($scope, $http) {
 
-	$scope.currentRoute;
-	$scope.currentLocation;
+	$scope.currentRoute = null;
+	$scope.currentLocation = null;
 
 	$scope.init = function() {
 		$http.get('scripts/schedule.json').success(function(data) {
 			$scope.data = data;
 
 			$scope.currentRoute = initRoute($scope.data);
-			$scope.currentLocation = initLocation($scope.currentRoute);	
+			$scope.currentLocation = initLocation($scope.currentRoute);
 		});
 	};
 
 	$scope.refreshLocationOnRouteChange = function() {
 		$scope.currentLocation = initLocation($scope.currentRoute);
-	}
+	};
 
 	var initRoute = function(data) {
 		return Object.keys(data)[0];
-	}
+	};
 
 	var initLocation = function(route) {
-		return Object.keys($scope.data[route])[0];
-	}
+		return $scope.data[route][0].name;
+	};
 
 	$scope.init();
 });
