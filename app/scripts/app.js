f8d4747ef8fc87cfd8c49a7915e3b794c181d2de
Bryant Pham <bryantphamm@gmail.com>
2014-08-03 15:49:21 -0500

    Added strikethrough styles on passed times

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -12,11 +12,42 @@ busBroApp.controller('busBroCtrl', function($scope, $http) {
 
 			$scope.currentRoute = initRoute($scope.data);
 			$scope.currentLocation = initLocation($scope.currentRoute);
+
 		});
 	};
 
 	$scope.refreshLocationOnRouteChange = function() {
 		$scope.currentLocation = initLocation($scope.currentRoute);
+	};
+
+	$scope.goToNextLocation = function(location) {
+		$scope.currentLocation = location;
+	};
+
+	$scope.strikeThroughOldTimes = function(timeString) {
+		var currentDate = new Date();
+		var currentHours = currentDate.getHours();
+  		var currentMinutes = currentDate.getMinutes();
+
+  		var hours = timeString.substr(0, timeString.indexOf(':'));
+  		var minutes = timeString.substr(timeString.indexOf(':')+1);
+  		var amOrPm = timeString.substr(timeString.length - 2);
+
+  		var hoursInt = parseInt(hours);
+  		var minutesInt = parseInt(minutes);
+
+  		if(amOrPm === 'PM' && hoursInt < 12) {
+  			hoursInt += 12;
+  		}
+
+  		if(currentHours > hoursInt) {
+  			return true;
+  		}
+		if(currentMinutes > minutesInt && currentHours === hoursInt) {
+			return true;
+		}
+
+		return false;
 	};
 
 	var initRoute = function(data) {
