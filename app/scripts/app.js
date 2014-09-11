44c01808aaefd60200a6be8390a1705f49fe6bb9
Bryant Pham <Bryantphamm@gmail.com>
2014-09-11 12:14:20 -0500

    Added Red route functionality

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -23,12 +23,31 @@ busBroApp.controller('busBroCtrl', function($scope, $http) {
 		$http.get('scripts/schedule.json').success(function(data) {
 			//Init scope variables
 			$scope.data = data;
-			$scope.model = $scope.data.Purple[$scope.locations[0]];
 			$scope.displayedLocationName = $scope.locations[0];
-			$scope.currentRoute = initRoute($scope.data);
-			$scope.transferRoute = initRoute($scope.data);
+			
+			if(isRedTime()) {
+				$scope.model = $scope.data.Red[$scope.locations[0]];
+				$scope.currentRoute = 'Red';
+				$scope.transferRoute = 'Red';
+			} else {
+				$scope.model = $scope.data.Purple[$scope.locations[0]];
+				$scope.currentRoute = 'Purple';
+				$scope.transferRoute = 'Purple';
+			}
 			$scope.borderColor = $scope.currentRoute;
 		});
+	};
+
+	var isRedTime = function() {
+		var currentTime = new Date();
+		var currentHour = currentTime.getHours();
+		var currentMinute =  currentTime.getMinutes();
+
+		//Force red route if between 12:00AM - 8:20AM and after 4:41PM
+		if((currentHour >= 0 && (currentHour <= 8 && currentMinute <= 20)) || (currentHour >= 16 && currentMinute >= 41)) {
+			return true;
+		}
+		return false;
 	};
 
 	$scope.refreshRoutesAndTimelist = function() {
@@ -39,33 +58,38 @@ busBroApp.controller('busBroCtrl', function($scope, $http) {
 
 	var refreshRoutes = function() {
 		//Determine if locations are on the same route
-		if($scope.data.Purple[$scope.currentLocation] && $scope.data.Purple[$scope.destinationLocation]) {
-			$scope.transferRoute = 'Purple';
-			$scope.currentRoute  = 'Purple';
-			$scope.borderColor = $scope.currentRoute;
-			return; 
-		} else if($scope.data.Blue[$scope.currentLocation] && $scope.data.Blue[$scope.destinationLocation]) {
-			$scope.transferRoute = 'Blue';
-			$scope.currentRoute  = 'Blue';
-			$scope.borderColor = $scope.currentRoute;
-			return; 
-		}
-
-		//Determine if locations are cross-route
-		if(!($scope.data[$scope.currentRoute][$scope.currentLocation])) {
-			if($scope.currentRoute === 'Purple') {
-				$scope.currentRoute = 'Blue';
-			} else {
-				$scope.currentRoute = 'Purple';
-			}
-		}
-
-		if(!($scope.data[$scope.currentRoute][$scope.destinationLocation])) {
-			if($scope.currentRoute === 'Purple') {
-				$scope.transferRoute = 'Blue';
-				$scope.transferLocation = $scope.data[$scope.transferRoute][$scope.destinationLocation].bestTransferLocation;
-			} else {
+		if(isRedTime()) {
+			$scope.transferRoute = 'Red';
+			$scope.currentRoute  = 'Red';
+		} else {
+			if($scope.data.Purple[$scope.currentLocation] && $scope.data.Purple[$scope.destinationLocation]) {
 				$scope.transferRoute = 'Purple';
+				$scope.currentRoute  = 'Purple';
+				$scope.borderColor = $scope.currentRoute;
+				return; 
+			} else if($scope.data.Blue[$scope.currentLocation] && $scope.data.Blue[$scope.destinationLocation]) {
+				$scope.transferRoute = 'Blue';
+				$scope.currentRoute  = 'Blue';
+				$scope.borderColor = $scope.currentRoute;
+				return; 
+			}
+
+			//Determine if locations are cross-route
+			if(!($scope.data[$scope.currentRoute][$scope.currentLocation])) {
+				if($scope.currentRoute === 'Purple') {
+					$scope.currentRoute = 'Blue';
+				} else {
+					$scope.currentRoute = 'Purple';
+				}
+			}
+
+			if(!($scope.data[$scope.currentRoute][$scope.destinationLocation])) {
+				if($scope.currentRoute === 'Purple') {
+					$scope.transferRoute = 'Blue';
+					$scope.transferLocation = $scope.data[$scope.transferRoute][$scope.destinationLocation].bestTransferLocation;
+				} else {
+					$scope.transferRoute = 'Purple';
+				}
 			}
 		}
 		$scope.borderColor = $scope.currentRoute;
@@ -102,10 +126,6 @@ busBroApp.controller('busBroCtrl', function($scope, $http) {
 		}
 
 		return false;
-	};
-
-	var initRoute = function(data) {
-		return Object.keys(data)[0];
 	};
 
 	$scope.displayLocationTimes = function(location) {
